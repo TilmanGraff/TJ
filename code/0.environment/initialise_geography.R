2da84d9341229e4c8c3b02edbc864597fb40fc73
TilmanGraff <tgraff@g.harvard.edu>
2021-01-05 14:09:21 +0100

    initialise

---
+++ code/0.environment/initialise_geography.R
@@ -0,0 +1,12 @@
+# init
+
+require("sp")
+require("Imap")
+require("gepaf")
+require("rgeos")
+require("rgdal")
+require("scales")
+
+setwd(".")
+
+# set up geography
---
+++ code/0.environment/test.py
@@ -0,0 +1,35 @@
+# prereqs
+
+import pandas as pd
+import numpy as np
+
+import geopandas as gp
+import geopy as gy
+import rasterio as ras
+
+import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib.patches import RegularPolygon
+#from matplotlib.collections import PatchCollection
+
+
+# define an environment
+
+xwidth = 10
+ywidth = 10
+radius = 1
+
+t = [range(1,10)]
+
+print(t)
+
+
+#for x in range
+# p = RegularPolygon((0,0), numVertices=6, radius=5, fill=False)
+#
+# fig, ax = plt.subplots()
+# ax.set_aspect('equal')
+#
+# ax.add_artist(p)
+# plt.axis([-10,10,-10,10])
+# plt.show()
