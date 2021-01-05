5bad0a4c4b394f255d11faa6536ec4088e0e9387
TilmanGraff <tgraff@g.harvard.edu>
2021-01-05 17:48:45 +0100

    first geography and route plotting

--- code/0.environment/initialise_geography.R
+++ code/0.environment/initialise_geography.R
@@ -7,6 +7,29 @@ require("rgeos")
 require("rgdal")
 require("scales")
 
-setwd(".")
+setwd("/Users/tilmangraff/Documents/GitHub/TJ")
 
 # set up geography
+# read in some border polygon
+borderpoly = SpatialPolygons(readOGR("./input/temp/testenvironment/borderpolygon/ng.shp")@polygons)
+
+HexPts <-spsample(borderpoly, type="hexagonal", n = 200, offset=c(0,0))
+
+n = length(HexPts)
+row.names(HexPts) = 1:n
+
+HexPols <- HexPoints2SpatialPolygons(HexPts)
+
+HexPolsDf = SpatialPolygonsDataFrame(HexPols, data = data.frame("ID" = 1:n, "x" = HexPts@coords[,1], "y" = HexPts@coords[,2]), match.ID = F)
+
+Vraw = as.matrix(dist(HexPts@coords, upper=TRUE)^(-3) * 100) + rnorm(n^2, sd = 10)
+V = matrix(0, n, n) + as.matrix(Vraw>0) * Vraw
+
+adj = as.numeric(gTouches(HexPolsDf, byid=TRUE))
+
+writeOGR(HexPolsDf,"./input/temp/testenvironment", "test",  driver="ESRI Shapefile", overwrite_layer=TRUE)
+write.csv(V, "./input/temp/testenvironment/V.csv")
+write.csv(adj, "./input/temp/testenvironment/adj.csv")
+#
+# plot(borderpoly)
+# plot(HexPols[borderpoly,])
---
+++ code/0.environment/plot_routes.R
@@ -0,0 +1,94 @@
+require("sp")
+require("rgdal")
+require("rgeos")
+
+setwd("/Users/tilmangraff/Documents/GitHub/TJ")
+
+colors = c("#B36305", "#E32017", "#FFD300", "#00782A", "#00A4A7", "#F3A9BB", "#A0A5A9", "#9B0056")
+
+hex = readOGR("./input/temp/testenvironment/test.shp")
+adj = gTouches(hex, byid=TRUE)
+
+n = length(hex)
+
+# find some random paths
+
+nlines = 6
+
+starts = sample(1:n, nlines)
+
+for(i in 1:nlines){
+  thisline = starts[i]
+  breaker = 0
+  while((length(thisline) < 3 | runif(1)<0.95) & breaker == 0){
+    currstation = thisline[length(thisline)]
+    nextstops = as.numeric(which(adj[currstation,], arr.ind = TRUE))
+    nextstops = nextstops[!nextstops %in% thisline]
+    if(length(nextstops) > 1){
+      newstop = sample(nextstops,1)
+      thisline = c(thisline, newstop)
+    }
+    if(length(nextstops) == 1){
+      newstop = nextstops
+      thisline = c(thisline, newstop)
+    }
+    if(length(nextstops) == 0){
+      breaker = 1
+    }
+  }
+  assign(paste0("line", i), thisline)
+}
+
+plot(hex, lwd = .5)
+#text(HexPts@coords, labels = 1:n, cex = .8)
+for(i in 1:nlines){
+  points(hex@data[get(paste0("line", i)), c("x","y")], type = "o", col = colors[i], lwd = 5, pch = 19)
+}
+
+# identify overlapping lines
+nd2trps = function(x){
+  x0 = c("", paste(x))
+  x1 = c(paste(x),"")
+  trps = vector()
+  for(i in 2:(length(x0)-1)){
+    trps = c(trps, paste(x0[i], x1[i], sep = "to"), paste(x1[i], x0[i], sep = "to"))
+  }
+  trps = trps[2:(length(trps)-1)]
+  return(trps)
+
+}
+
+# identify hubs
+alllines = vector()
+alltrips = vector()
+for(i in 1:nlines){
+  alllines = c(alllines, get(paste0("line", i)))
+  alltrips = c(alltrips, nd2trps(get(paste0("line", i))))
+}
+
+trunktrps = alltrips[which(duplicated(alltrips))]
+
+# draw trunktrips
+for(thistrp in trunktrps){
+  to = as.numeric(gsub(".*to", "", thistrp))
+  from = as.numeric(gsub("to.*", "", thistrp))
+
+  if(to > from){
+  whichlines = vector()
+  for(i in 1:nlines){
+    if(thistrp %in% nd2trps(get(paste0("line", i)))){
+      whichlines = c(whichlines, i)
+    }
+  }
+
+  lineseq.n = length(whichlines)
+  for(lineseg in 1:lineseq.n){
+    thislineseg = whichlines[lineseg]
+    points(hex@data[c(to,from), c("x")], hex@data[c(to,from), c("y")], type = "l", col = colors[thislineseg], lwd = 15-12/(1-lineseq.n) + lineseg*12/(1-lineseq.n))
+  }
+}
+}
+
+
+hubs = alllines[which(duplicated(alllines))]
+points(hex@data[hubs, c("x","y")], col = "black", pch = 21, cex = 2, bg = "white")
