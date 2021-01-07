1e49074097c9ccc47c9c252a282381ab28baa0b3
TilmanGraff <tgraff@g.harvard.edu>
2021-01-07 17:54:35 +0100

    simplest welfare version

--- code/0.environment/read_env.py
+++ code/0.environment/read_env.py
@@ -8,14 +8,24 @@ os.chdir("/Users/tilmangraff/Documents/GitHub/TJ")
 
 Vraw = np.array(pd.read_csv("./input/temp/testenvironment/V.csv"))
 adj = np.array(pd.read_csv("./input/temp/testenvironment/adj.csv"))
+dist = np.array(pd.read_csv("./input/temp/testenvironment/dist.csv"))
 
-n = len(Vraw)-1
+n = len(Vraw)
 
 # create dictionaries
 V = {}
 nb = {}
+cdrive = {}
+bdrive = {}
+cdrivemat = dist
 
 for i in range(n):
     my_dict = dict(zip(range(n), Vraw[i,:]))
     V[i] = my_dict
     nb[i] = list(np.array(np.where(adj[i,:]==1)).flat)
+
+    my_dict = dict(zip(range(n), dist[i,:]))
+    cdrive[i] = my_dict
+
+    my_dict = dict(zip(range(n), dist[i,:]*0.5)) # lets just say for now you can go double as fast by bus...
+    bdrive[i] = my_dict
