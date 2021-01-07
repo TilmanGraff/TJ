5904c428170909207980cc805178b242dea8c3ec
TilmanGraff <tgraff@g.harvard.edu>
2021-01-07 19:23:19 +0100

    v1.5

--- code/1.computewelfare/define_functions.py
+++ code/1.computewelfare/define_functions.py
@@ -11,16 +11,16 @@ os.chdir("/Users/tilmangraff/Documents/GitHub/TJ")
 exec(open("./code/0.environment/read_env.py").read())
 
 # initialise some line
-bl = [0, 2, 7, 8, 15, 25, 37, 51, 52, 66, 53]
+bl = [[0, 2, 7, 8, 15, 25, 37, 51, 52, 66, 53], 50] # lines are always a tupel of an array (number of stops) and an integer (number of busses)
 
 # construct linkdists
 def compute_linkdists(bl):
-    return [bdrive[bl[i]][bl[i+1]] for i in range((len(bl)-1))]
+    return [bdrive[bl[0][i]][bl[0][i+1]] for i in range((len(bl[0])-1))]
 
 # construct travel times between connected cells (add linkdists later)
 def compute_busline(bl, linkdists = compute_linkdists(bl)):
-    dto1 = [sum(linkdists[0:i]) for i in range(len(bl))]
-    stops = np.asarray([np.abs(np.subtract(dto1, dto1[i])) for i in range(len(bl))])
+    dto1 = [sum(linkdists[0:i]) for i in range(len(bl[0]))]
+    stops = np.asarray([np.abs(np.subtract(dto1, dto1[i])) for i in range(len(bl[0]))])
     return stops
 
 
@@ -28,11 +28,11 @@ def compute_busline(bl, linkdists = compute_linkdists(bl)):
 def compute_direct_connections_route(bl):
     # construct direct connections matrix D
     D = np.zeros((n,n), dtype=int)
-    D[bl,:] = 1 # fill entire lines of connected nodes
+    D[bl[0],:] = 1 # fill entire lines of connected nodes
     D = D * D.T # multiply by transpose to identify connected cells
 
     C = np.full((n,n), float("inf"))
-    C[np.array(bl)[:,np.newaxis], bl] = compute_busline(bl)
+    C[np.array(bl[0])[:,np.newaxis], bl[0]] = compute_busline(bl)
 
     return C,D
 
